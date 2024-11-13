7e400e66474129333b46009ccd82d50897c82c5f
raghavaflm <raghuat1991@gmail.com>
2024-11-13 19:58:22 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -8,3 +8,4 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
 systemctl start jenkins
 systemctl status jenkins
+update-alternatives --config java
