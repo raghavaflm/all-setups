852bf651577ca226ea8cae6b30d9238c55e462aa
raghavaflm <raghuat1991@gmail.com>
2024-11-13 19:24:58 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -13,3 +13,18 @@ update-alternatives --config java
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
 systemctl start jenkins.service
 systemctl status jenkins.service
+
+
+
+
+updated
+yum install java-1.8.0-openjdk -y
+yum install maven -y
+mvn -v
+yum install java-17-amazon-corretto -y
+java -version
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl start jenkins
+systemctl status jenkins
