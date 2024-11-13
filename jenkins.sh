572914e8f3ba3d243635aab0ce90c8105665fc5f
raghavaflm <raghuat1991@gmail.com>
2024-11-13 19:27:20 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,10 @@
-#STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
-yum install git java-1.8.0-openjdk maven -y
-
-#STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
+yum install java-1.8.0-openjdk -y
+yum install maven -y
+mvn -v
+yum install java-17-amazon-corretto -y
+java -version
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-#STEP-3: DOWNLOAD JAVA11 AND JENKINS
-yum install java-17-amazon-corretto -y
 yum install jenkins -y
-update-alternatives --config java
-
-#STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
-systemctl start jenkins.service
-systemctl status jenkins.service
+systemctl start jenkins
+systemctl status jenkins
